6f9a1a57578d6cc5f5b986c9af5e4c9da3f0a44b
rajdalal1109 <dalalraj1109@gmail.com>
2024-03-06 09:23:34 +0530

    6/3/24 9:23

--- lib/UI/profile.dart
+++ lib/UI/profile.dart
@@ -1,24 +1,144 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:project/UI/setting.dart';
 import 'package:project/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Profile extends StatefulWidget {
-  const Profile({super.key});
+  const Profile({Key? key}) : super(key: key);
 
   @override
   State<Profile> createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  Uint8List? _image;
+  File? selectedIMage;
+
+
+  void showImagePickerOption(BuildContext context) {
+    showModalBottomSheet(
+        backgroundColor: const Color.fromRGBO(255, 98, 96, 1),
+        context: context,
+        builder: (builder) {
+          return Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: SizedBox(
+              height: 100,
+              width: 400,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        _getGallery();
+                      },
+                      child: const SizedBox(
+                        child: Column(
+                          children: [
+                            Icon(Icons.image, size: 50,color: Colors.white,),
+                            Text("Gallery",style: TextStyle(color: Colors.white),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        _getCamera();
+                      },
+                      child: const SizedBox(
+                        child: Column(
+                          children: [
+                            Icon(Icons.camera_alt, size: 50,color: Colors.white,),
+                            Text("Camera",style: TextStyle(color: Colors.white),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+//Gallery
+  Future _getGallery() async {
+    final returnImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (returnImage == null) return;
+    setState(() {
+      selectedIMage = File(returnImage.path);
+      _image = File(returnImage.path).readAsBytesSync();
+    });
+
+    // Save image to local storage
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('profile_image', base64Encode(_image!));
+    Navigator.of(context).pop();
+  }
+
+//Camera
+  Future _getCamera() async {
+    final returnImage = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (returnImage == null) return;
+    setState(() {
+      selectedIMage = File(returnImage.path);
+      _image = File(returnImage.path).readAsBytesSync();
+    });
+
+    // Save image to local storage
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('profile_image', base64Encode(_image!));
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImage();
+  }
+
+  Future<void> _loadImage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? profileImageString = prefs.getString('profile_image');
+
+    if (profileImageString != null) {
+      setState(() {
+        _image = base64Decode(profileImageString);
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile",style: TextStyle(color: Colors.white),),
-        backgroundColor:  const Color.fromRGBO(255, 98, 96, 1),
-        surfaceTintColor:  const Color.fromRGBO(255, 98, 96, 1),
+        title: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromRGBO(255, 98, 96, 1),
+        surfaceTintColor: const Color.fromRGBO(255, 98, 96, 1),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showImagePickerOption(context);
+            },
+            icon: const Icon(
+              Icons.add_a_photo_outlined,
+              color: Colors.white,
+              size: 30,
+            ),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
@@ -26,70 +146,85 @@ class _ProfileState extends State<Profile> {
         child: SizedBox(
           height: MediaQuery.of(context).size.height,
           child: Stack(
-              clipBehavior: Clip.none,
+            clipBehavior: Clip.none,
             children: [
               Container(
                 height: 179,
-                  width: 393,
-                  decoration:  const BoxDecoration(
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(8),bottomRight: Radius.circular(8)),
-                    color: Color.fromRGBO(255, 98, 96, 1),
+                width: 393,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8),
                   ),
+                  color: Color.fromRGBO(255, 98, 96, 1),
                 ),
-                Positioned(
-                  top: 100,
-                  left: 20,
-                  right: 20,
-                  child: Column(
-                    children: [
-                      Image.asset("assets/images/girldp2.png",),
-                      const SizedBox(height: 8,),
-                      const Text("Carla Dakota",style: TextStyle(fontWeight: FontWeight.w800,fontSize: 25),),
-
-                      const SizedBox(height: 30,),
-                      const Card(
+              ),
+              Positioned(
+                top: 80,
+                left: 20,
+                right: 20,
+                child: Column(
+                  children: [
+                    if (_image != null)
+                      CircleAvatar(radius: 90, backgroundImage: MemoryImage(_image!),)
+                    else
+                      const CircleAvatar(radius: 90, backgroundImage: AssetImage("assets/images/girldp2.png"),),
+                    const SizedBox(height: 8),
+                    const Text(
+                      "Carla Dakota",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800, fontSize: 25),
+                    ),
+                    const SizedBox(height: 30),
+                    const Card(
+                      child: ListTile(
+                        title: Text("Email", style: TextStyle(fontSize: 16,
+                            fontWeight: FontWeight.w500)),
+                        subtitle: Text("abc@gmail.com", style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400)),
+                        trailing: Icon(Icons.email_outlined),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    const Card(
+                      child: ListTile(
+                        title: Text("Phone Number", style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500)),
+                        subtitle: Text("+91\t1234567890", style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400)),
+                        trailing: Icon(Icons.local_phone_outlined),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Setting(),));
+                      },
+                      child: const Card(
                         child: ListTile(
-                          title: Text("Email",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500)),
-                          subtitle: Text("abc@gmail.com",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w400)),
-                          trailing: Icon(Icons.email_outlined),
+                          title: Text("Setting", style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500)),
+                          trailing: Icon(Icons.settings_outlined),
                         ),
                       ),
-                      const SizedBox(height: 5,),
-                      const Card(
+                    ),
+                    const SizedBox(height: 5),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => LogIn(),));
+                      },
+                      child: const Card(
                         child: ListTile(
-                          title: Text("Phone Number",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500)),
-                          subtitle: Text("+91\t1234567890",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w400)),
-                          trailing: Icon(Icons.local_phone_outlined),
+                          title: Text("Log Out", style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500)),
+                          trailing: Icon(Icons.power_settings_new_outlined),
                         ),
                       ),
-                      const SizedBox(height: 5,),
-                      GestureDetector(
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const Setting(),));
-                        },
-                        child: const Card(
-                          child: ListTile(
-                            title: Text("Setting",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500)),
-                            trailing: Icon(Icons.settings_outlined),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5,),
-                      GestureDetector(
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const LogIn(),));
-                        },
-                        child: const Card(
-                          child: ListTile(
-                            title: Text("Log Out",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500)),
-                            trailing: Icon(Icons.power_settings_new_outlined),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-            ]
+              ),
+            ],
           ),
         ),
       ),
