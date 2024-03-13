0ccea495f8a1ca41f3a6b56b61b56cb73aa8ebd7
rajdalal1109 <dalalraj1109@gmail.com>
2024-03-13 23:48:34 +0530

    13/3/2024 11:48 appname change

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: "Ubuntu"),
-      title: 'Bus Buddy',
+      title: 'BusBuddy',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: "Bus Buddy"),
+      home: MyHomePage(title: "BusBuddy"),
     );
   }
 }
